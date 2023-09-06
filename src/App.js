f0f9fe31f234ba0c989b8d1ee470eb72eda618eb
GaziRiad <ouhibimustapha36@gmail.com>
2023-09-06 13:59:27 +0100

    setup with useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+
+  // 'loading, 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch(`http://localhost:8000/questions`);
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    fetchQuestions();
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main className="main">
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
